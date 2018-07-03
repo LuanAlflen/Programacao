dd10ae95498fbf9f7a78e3b2102957d20ed9144d
LuanAlflen <luan.alflen4@gmail.com>
2018-07-03 16:58:07 -0300

    Add files via upload

--- Redes_e_Programação/app/Controler/ControlerIP.php
+++ Redes_e_Programação/app/Controler/ControlerIP.php
@@ -21,13 +21,26 @@ if (isset($_GET['acao'])){
             $ip2 = $_POST['segundoip'];
             $ip3 = $_POST['terceiroip'];
             $ip4 = $_POST['quartoip'];
-            $mascara = $_POST['mascara'];
+            $decimal = $_POST['mascara'];
 
             $ip = "$ip1".'.'."$ip2".'.'."$ip3".'.'."$ip4";
-            $teste = new IP($ip,$mascara);
+            $teste = new IP($ip,$decimal);
             $crud = new IPCrud();
-            $numeroRedes = $crud->numerodeRedes($mascara);
-            echo "IP: $ip/$mascara, Mascara: null,Numero de Redes: $numeroRedes";
+            $numeroRedes = $crud->hostsEmCadaRede($decimal);
+            $subredes = $crud->subredes($numeroRedes);
+            $mascara = $crud->mascara($decimal);
+            $classe = $crud->classeIP($ip1);
+            $publicoOuPrivado = $crud->privadoOuPublico($ip1,$ip2);
+            echo "IP: $ip/$decimal. \n
+            Mascara: $mascara. \n
+            A quantidade de sub-redes:$subredes  \n
+            A quantidade de endereços por sub-rede; $numeroRedes.,
+            A quantidade de endereços de hosts em cada sub-rede: ,
+            Mascara em decimal: $decimal,
+            Classe: $classe,
+            Publico ou Privado: $publicoOuPrivado.
+            
+            ";
 
             break;
     }
